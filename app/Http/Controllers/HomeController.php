be7eb193c346fdd9b1ea71b4446195baf604dba2
Leandro Rolim <leandro.rolim@troc.com.br>
2021-08-24 17:47:33 -0300

    psalm fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __invoke($nome, Request $request)
+     public function __invoke(string $nome, Request $request)
     {
         return "bem vindo $nome";
     }
