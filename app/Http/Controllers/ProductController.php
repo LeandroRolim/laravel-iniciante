0cc4115f56d6cb4145978ea7b89fda457dbd5343
Leandro Rolim <leandro.rolim@troc.com.br>
2021-08-26 18:06:54 -0300

    add product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     */
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->value = $request->input('value');
+        $product->description = $request->input('description');
+        $product->status = $request->input('status');
+        $product->save();
+        return $product;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     */
+    public function update(Request $request, Product $product)
+    {
+        $product->value = $request->input('value');
+        $product->save();
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return 'deleted';
+    }
+}
