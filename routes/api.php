6dbf4927870588cc9f21f2c59907f71acb79190e
Leandro Rolim <leandro.rolim@troc.com.br>
2021-08-29 15:18:59 -0300

    add product

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/', fn(Request $request) => $request->input('filter', ''))->name('');
+
+Route::resource('product', ProductController::class);
